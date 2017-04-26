ffeb36e1e3d151d0d8107d2ab81887aabb05a76b
Antoniolm <codiak19@gmail.com>
2017-04-26 12:34:53 +0200

    The user can be registered in our system

--- redsocialII/start.php
+++ redsocialII/start.php
@@ -1,14 +1,17 @@
+<?php 
+	include("conexionBD.php"); 
+?>
 <body>
 	<section class="MainSection">
 		<img id="imageMainSectionLeft" src="./img/faceface.png"/>
 	</section>
 	<section class="MainSection">
 	<h3>¿Quieres unirte a nosotros?</h3>
-	<form id="registerForm" action="">
-		Nombre:<input type="text" required/><br><br>
-		Apellido:<input type="text" required/><br><br>
-		Nombre en la red:<input type="text" required/><br><br>
-		Contraseña:<input type="text" required/><br><br>
+	<form id="registerForm" action="./script/script_new_user.php" method="post">
+		Nombre:<input type="text" id="name"  name="name" required/><br><br>
+		Apellido:<input type="text" id="lastname"  name="lastname" required/><br><br>
+		Nombre en la red:<input type="text" id="nickname"  name="nickname" required/><br><br>
+		Contraseña:<input type="text" id="password"  name="password" required/><br><br>
 		Repite la contraseña:<input type="text" required/><br><br>
 		<input type="submit" value="login" /><br><br>	
 	</form>
